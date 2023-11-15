73705f7254bcab7fe0d1bd83244623993c6ce48f
Ana Brendel <anabrendel25@gmail.com>
2023-11-15 01:21:15 -0800

    finish running the sort tests

--- helper.py
+++ helper.py
@@ -23,23 +23,25 @@ for t in os.listdir(main):
         testFolders.append(t)
 
 benches = [
-    "sort_lemma_insert_sorted_IN_sort_sorted",
-    "sort_lemma_insert_perm_IN_sort_perm",
-    "sort_lemma_sort_perm_IN_insertion_sort_correct",
-    "sort_lemma_sort_sorted_IN_insertion_sort_correct",
+    #"sort_lemma_insert_sorted_IN_sort_sorted",
+    #"sort_lemma_insert_perm_IN_sort_perm",
+    #"sort_lemma_sort_perm_IN_insertion_sort_correct",
+    #"sort_lemma_sort_sorted_IN_insertion_sort_correct",
     "sort_lemma_sorted_remove_cons_1_IN_insert_sortedd",
-    "sort_lemma_sorted_remove_cons_2_IN_insert_sortedd",
+    #"sort_lemma_sorted_remove_cons_2_IN_insert_sortedd",
     "sort_lemma_sorted_remove_cons_3_IN_insert_sortedd",
-    "sort_lemma_sorted_remove_cons_4_IN_insert_sortedd",
-    "sort_lemma_sorted_remove_cons_5_IN_insert_sortedd",
-    "sort_lemma_sorted_remove_1_IN_insert_sortedd",
-    "sort_lemma_sorted_remove_2_IN_insert_sortedd",
-    "sort_lemma_insert_sortedd_IN_sort_sortedd"
+    #"sort_lemma_sorted_remove_cons_4_IN_insert_sortedd",
+    #"sort_lemma_sorted_remove_cons_5_IN_insert_sortedd",
+    #"sort_lemma_sorted_remove_1_IN_insert_sortedd",
+    #"sort_lemma_sorted_remove_2_IN_insert_sortedd",
+    #"sort_lemma_insert_sortedd_IN_sort_sortedd"
 ]
 
 decide = "/home/anabrendel/lfind/vfa-benchmarks/Sort/benchmarks/sources/sort_lemma_sorted_remove_cons_1_IN_insert_sortedd/decide.v"
 src = "/home/anabrendel/lfind/vfa-benchmarks/Sort/benchmarks/sources"
 
 for t in os.listdir(src):
-    test = os.path.join(src,t)
-    make(test)
+    if t in benches:
+        print(t)
+        test = os.path.join(src,t)
+        make(test)
